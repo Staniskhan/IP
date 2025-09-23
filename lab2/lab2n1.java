b3134cb516f8f4224c0e236efe1498c67cede499
Staniskhan <andromedas82000@gmail.com>
2025-09-23 14:07:53 +0300

    delete comments from lab2 num1

--- lab2/lab2n1.java
+++ lab2/lab2n1.java
@@ -51,43 +51,5 @@ public class lab2n1 {
         {
             System.out.println("\nmax repeating element in matrix: " + maxRepEl + "\n");
         }
-        //-----------------------------------------------
-        // boolean indicator = false;
-        // int count;
-        // for (int k = maxVal; k >= minVal; k--)
-        // {
-        //     count = 0;
-        //     for(int i = 0; i < rows; i++)
-        //     {
-        //         for (int j = 0; j < columns; j++)
-        //         {
-        //             if (matrix[i][j] == k)
-        //             {
-        //                 count++;
-        //             }
-        //             if (count == 2)
-        //             {
-        //                 System.out.println("\n" + matrix[i][j] + "\n");
-        //                 indicator = true;
-        //                 break;
-        //             }
-        //         }
-        //         if (indicator)
-        //         {
-        //             break;
-        //         }
-        //     }
-        //     if (indicator)
-        //     {
-
-        //         break;
-        //     }
-        // }
-
-        // if (!indicator)
-        // {
-        //     System.out.println("\nThere are no repeating numbers in the matrix\n");
-        // }
-
     }
 }
