26b4793f6d18abbe12a73d71ba19a296df5e3c22
Staniskhan <andromedas82000@gmail.com>
2025-10-07 13:18:43 +0300

    redo the structure of project, change .gitignore

--- lab4/lab4.java
+++ lab4/lab4.java
@@ -1,26 +1,26 @@
 
 public class lab4 {
-    public void main(String args[])
+    public static void main(String args[])
     {
-        // CRTest.generateFile("lab4_input.txt", 10, 5, 4, 7);
-        CollectionRuler clctrlr = new CollectionRuler("lab4_input.txt");
-        clctrlr.fileOutAllInformation("allInf.txt");
-        clctrlr.fileOutNames("names.txt");
-        clctrlr.fileOutAVG("AVG.txt");
+        CRTest.generateFile("input_files/lab4_input.txt", 10, 5, 4, 7);
+        CollectionRuler clctrlr = new CollectionRuler("input_files/lab4_input.txt");
+        clctrlr.fileOutAllInformation("output_files/allInf.txt");
+        clctrlr.fileOutNames("output_files/names.txt");
+        clctrlr.fileOutAVG("output_files/AVG.txt");
         clctrlr.sortAVGUp();
-        clctrlr.fileOutAVG("sortUpAVG.txt");
+        clctrlr.fileOutAVG("output_files/sortUpAVG.txt");
         clctrlr.sortAVGDown();
-        clctrlr.fileOutAVG("sortDownAVG.txt");
+        clctrlr.fileOutAVG("output_files/sortDownAVG.txt");
         clctrlr.sortNameUp();
-        clctrlr.fileOutNames("sortUpName.txt");
+        clctrlr.fileOutNames("output_files/sortUpName.txt");
         clctrlr.sortNameDown();
-        clctrlr.fileOutNames("sortDownName.txt");
+        clctrlr.fileOutNames("output_files/sortDownName.txt");
         clctrlr.sortYearGroupUp();
-        clctrlr.fileOutAllInformation("sortUpYearGroup.txt");
+        clctrlr.fileOutAllInformation("output_files/sortUpYearGroup.txt");
         clctrlr.sortYearGroupDown();
-        clctrlr.fileOutAllInformation("sortDownYearGroup.txt");
-        clctrlr.fileOutExcellentStudentsAllInf("ExcellentStudentsAllInf.txt");
-        clctrlr.fileOutExcellentStudentsNames("ExcellentStudentsNames.txt");
-        clctrlr.fileOutExcellentStudentsAVG("ExcellentStudentsAVG.txt");
+        clctrlr.fileOutAllInformation("output_files/sortDownYearGroup.txt");
+        clctrlr.fileOutExcellentStudentsAllInf("output_files/ExcellentStudentsAllInf.txt");
+        clctrlr.fileOutExcellentStudentsNames("output_files/ExcellentStudentsNames.txt");
+        clctrlr.fileOutExcellentStudentsAVG("output_files/ExcellentStudentsAVG.txt");
     }
 }
