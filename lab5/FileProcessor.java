6fd915bb4b319baa8884a5c9e468f4067e135b4d
Staniskhan <andromedas82000@gmail.com>
2025-11-18 16:39:17 +0300

    modifiy lab5. add method returning one formatted paragraph as a string (paragraphStr(Vector<String>))

--- lab5/FileProcessor.java
+++ lab5/FileProcessor.java
@@ -1,5 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.Vector;
 
@@ -69,31 +71,82 @@ public class FileProcessor {
             return;
         }
 
-        try (Scanner scanner = new Scanner(new File("input.txt"))) {
+        Vector<String> text = new Vector<>();
+        try (Scanner scanner = new Scanner(new File("input_files/input.txt"))) {
             Vector<String> words = new Vector<>();
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                if (!line.startsWith("\t"))
+                if (line.startsWith(" ") && !words.isEmpty())
                 {
-                    String[] lineWords = line.split("\\s+");
-                    for (int i = 0; i < lineWords.length; i++)
-                    {
-                        words.add(lineWords[i]);
-                    }
-                }
-                else
-                {
-                    //displayParagraph(words)
+                    text.add(paragraphStr(words));
                     words.clear();
-                    String[] lineWords = line.split("\\s+");
-                    for (int i = 0; i < lineWords.length; i++)
-                    {
-                        words.add(lineWords[i]);
-                    }
+                }
+                String[] lineWords = line.split("\\s+");
+                for (int i = 0; i < lineWords.length; i++)
+                {
+                    words.add(lineWords[i]);
                 }
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
+        try(FileWriter writer = new FileWriter("output_files/output.txt"))
+        {
+            for (int i = 0; i < text.size(); i++)
+            {
+                writer.write(text.get(i));
+            }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    private String paragraphStr(Vector<String> words)
+    {
+        int len = words.get(0).length() + 4;                            // +4?
+        int number_of_words = 1;
+        String par = "\t";
+
+
+        while(!words.isEmpty())
+        {
+
+            while (words.size() > number_of_words && len + words.get(number_of_words).length() + 1 <= width)
+            {
+                len += 1 + words.get(number_of_words).length();
+                number_of_words++;
+            }
+            int size_of_ws = 0;
+            if (number_of_words > 1)
+            {
+                size_of_ws = (width - len + number_of_words - 1) / (number_of_words - 1);
+            }
+            String ws = "";
+            for (int i = 0; i < size_of_ws; i++)
+            {
+                ws += " ";
+            }
+
+            par += words.get(0);
+            words.remove(0);
+            for (int i = 0; i < number_of_words && i < words.size(); i++)
+            {
+                par += ws + words.get(0);
+                words.remove(0);
+            }
+            par += "\n";
+            if (!words.isEmpty())
+            {
+                len = words.get(0).length();
+            }
+            number_of_words = 1;
+        }
+
+
+
+        return par;
     }
 }
--- lab5/lab5.java
+++ lab5/lab5.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class lab5 {
     public static void main (String args[])
     {
-        //System.out.println(FileProcessor.tabs("input.txt"));
+        Scanner scanner = new Scanner (System.in);
+        int w = scanner.nextInt();
+        FileProcessor fp = new FileProcessor("input_files/input.txt", "output_files/output.txt", w);
+        fp.process();
     }
 }
