a9720bfe786690d64e5150bf5d3c45c69930a475
Staniskhan <andromedas82000@gmail.com>
2025-12-23 12:23:51 +0300

    change pom.xml in library project

--- library/src/main/java/com/staniskhan/library/model/Book.java
+++ library/src/main/java/com/staniskhan/library/model/Book.java
@@ -2,7 +2,7 @@ package com.staniskhan.library.model;
 
 import java.math.BigDecimal;
 
-import jakarta.persistence.Column;
+import jakarta.persistence .Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
